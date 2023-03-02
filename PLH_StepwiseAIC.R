618d8d38a50b679da254071572e0a5a0137cb1e5
robclark19 <robert.e.clark@wsu.edu>
2023-03-02 10:41:17 -0500

    Make example figures for model 4.

--- PLH_StepwiseAIC.R
+++ PLH_StepwiseAIC.R
@@ -1,8 +1,19 @@
-
+# Notes ####
 # Script for using Stepwise AIC for discovering a parsimonous mixed model with potato leaf hopper data
 # Data from midwestern suction trap network provided by Mike Crossley and collaborators
 
-# Libraries
+# Tips from Rob on 3/2/2023
+# All data is imported like prior scripts, but check to make sure I made the correct assumptions
+# Model averaging in this context would be a serious effort, perhaps not yet warranted.
+# model.avg() example code is retained for model 1.
+# stepAIC() is flexible and the models are easily changed to include interactions
+# You can force stepAIC() to keep certain independent variables with the "keep =" statement
+# More data & time would be needed for lasso/ridge regression, so that's ruled out for now
+
+# In a larger project we could write functions to automatically take the trimmed model and provide outputs
+# For now the trimmed models have to be entered manually when inspecting output from stepAIC(model)$anova
+
+# Libraries ####
 library("tidyverse")
 library("janitor")
 library("lme4")
@@ -14,15 +25,17 @@ library("nlme")
 library("MASS") #stepAIC()
 library("glmmTMB")
 library("MuMIn") # model.avg(). Rob has no experience with this approach so proceed with caution.
+library("ggeffects") # produces model predictions for ggplot
+library("ggpubr") # tools for making pub figures
 
-# Import raw data
+# Import raw data ####
 
 plh_dat = read.table('PLH_pheno_weather_land.txt',sep='\t',as.is=T,check.names=F,header=T) %>%
   clean_names() %>%
   mutate_at(vars(year,hoppers_week), factor) %>%
   mutate_at(vars(gdd_total), function(x) x/1000) # scale down growing degree days
 
-# Trimming methods following original script
+# Trimming methods following original script ####
 # Trim site*years with low sample coverage, Chase, & sites lacking first or last captures
 threshold = 15 # cutoff
 
@@ -33,7 +46,7 @@ plh_dat <- plh_dat %>%
   subset(., last > 213)
 
 
-# Model 1
+# Model 1 ####
 # Effects on number of potato leafhoppers
 
 # Fully specified model using all independent variables seen in original script
@@ -54,10 +67,22 @@ mod1_trim <- glmmTMB(n_hoppers ~ lat + p_beans, data = plh_dat)
 Anova(mod1_trim)
 summary(mod1_trim) # more or less looks like only beans matter
 
+# Model averaging test case ####
+# https://stats.stackexchange.com/questions/208724/interpreting-model-averaging-results-in-r
+# https://search.r-project.org/CRAN/refmans/MuMIn/html/model.avg.html
+
+# pick two models with similar AIC reported from stepAIC output
+mod1_a<- glmmTMB(n_hoppers ~ lat + p_beans + n_week, data = plh_dat)
+mod1_b <- glmmTMB(n_hoppers ~ lat + p_beans + p_potato, data = plh_dat)
+
+# make a model list and then report summary of conditional models
+mod_list <- list(mod1_a,mod1_b)
+model.avg(mod_list) %>% summary()
 
 
 
-# Model 2
+
+# Model 2 ####
 # Effects on date of first capture
 
 mod2_full <- glmmTMB(first ~ year + n_week + lat + p_alfalfa + p_potato + p_beans + tmean_summer,
@@ -77,7 +102,7 @@ summary(mod2_trim)
 
 
 
-# Model 3
+# Model 3 ####
 # Effects on date of last capture
 
 mod3_full <- glmmTMB(last ~ year + n_week + lat + p_alfalfa + p_potato + p_beans + tmean_summer,
@@ -94,7 +119,7 @@ Anova(mod3_trim)
 summary(mod3_trim)
 
 
-# Model 4
+# Model 4 ####
 # Effects on the range between first and last
 
 mod4_full <- glmmTMB(range ~ year + n_week + lat + p_alfalfa + p_potato + p_beans + tmean_summer,
@@ -109,3 +134,35 @@ mod4_trim <- glmmTMB(range ~ year + lat + p_potato, data = plh_dat)
 
 Anova(mod4_trim)
 summary(mod4_trim)
+
+
+# Example Data Viz ####
+
+mod4_pred = ggpredict(mod4_trim, ci.lvl=NA)
+
+mod4_plot_lat = mod4_pred$lat %>%
+  ggplot(aes(x=x,y=predicted)) +
+  geom_point(data=plh_dat, aes(x=lat, y=range), size=0.5, alpha=0.5) + # pull points from raw data
+  geom_line() +
+  # scale_y_continuous(limits=c(40,160)) + # trim lower ones from viz
+  labs(x = "Latitude", y = "Date Range of Planthopper Observations")
+
+mod4_plot_lat
+
+mod4_plot_tater = mod4_pred$p_potato %>%
+  ggplot(aes(x=x,y=predicted)) +
+  geom_point(data=plh_dat, aes(x=p_potato, y=range), size=0.5, alpha=0.5) + # pull points from raw data
+  geom_line() +
+  # scale_y_continuous(limits=c(40,160)) + # trim lower ones from viz
+  labs(x = "Proportional coverage of nearby potato", y = "Date Range of Planthopper Observations")
+
+mod4_plot_tater
+
+# arrange and output as png for pubs
+fig_1ab <- ggarrange(mod4_plot_lat, mod4_plot_tater, labels = c("a", "b"), nrow = 1,
+                     common.legend = FALSE, widths = c(1, 1))
+
+ggsave(filename = "./figures/fig_1ab.png", plot = fig_1ab , device = "png",
+       width = 10, height = 4, units = "in", scale = 0.9)
+
+
